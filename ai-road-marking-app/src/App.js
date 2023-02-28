216255f37a36dacb5d3532d202b84f3ac25f7e8c
PhoenixIIexe <sashaerg10.07.6@mail.ru>
2023-03-01 00:58:48 +0300

    start

---
+++ ai-road-marking-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Main from './components/component-main/index'
+import './App.css';
+
+
+class App extends Component {
+    render() {
+        return (
+            <Main />
+        );
+    }
+}
+
+export default App;
---
+++ ai-road-marking-app/src/components/component-main/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+
+import mingcuteRoadLine from '../../image/mingcute-road-line.svg';
+import mingcuteRoadLineWhite from '../../image/mingcute-road-line_white.svg';
+import importImage from '../../image/import-image.svg';
+import uilImageDownload from '../../image/uil-image-download.svg'
+
+import './component-main.css';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className='page'>
+                <header className='header'>
+                    <div className='header_log-app log-app'>
+                        <p className='log-app__name'>
+                            AI. Road Marking
+                        </p>
+                        <img src={mingcuteRoadLine} className='log-app__img' />
+                    </div>
+                    <nav className='header__navigation navigation'>
+                        <a className='navigation__calibration button--blue button'>
+                            Калибровка
+                        </a>
+
+                        <a className='navigation__language button--blue button'>
+                            🇬🇧
+                        </a>
+                    </nav>
+                </header>
+
+                <main className='main'>
+                    <div className='main__log-app'>
+                        <p className='log-app__name--white'>
+                            AI. Road Marking
+                        </p>
+                        <img src={mingcuteRoadLineWhite} className='log-app__img' />
+                    </div>
+                    <div className='main__description description'>
+                        Приложение для выделения важных объектов дорожной ситуации.
+                    </div>
+                    <div className='main__import-image import-image'>
+                        <input type='file' accept='image/png, image/jpeg' className='import-image__file'></input>
+                        <img src={importImage} />
+                        Перетащите изображение или нажмите на кнопку
+                    </div>
+                    <div className='export-image'>
+                        <div className='export-image__download-image'>
+                            <img src={uilImageDownload} />
+                            Скачать
+                        </div>
+                    </div>
+                </main>
+            </div>
+        );
+    }
+}
+
+export default Main;
