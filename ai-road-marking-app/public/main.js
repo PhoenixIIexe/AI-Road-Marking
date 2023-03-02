c63b4c7b7021972a365380c6f51b099e392a2c3c
PhoenixIIexe <sashaerg10.07.6@mail.ru>
2023-03-02 11:06:05 +0300

    build

---
+++ ai-road-marking-app/public/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+const isDev = require('electron-is-dev')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        },
+        frame: true
+    })
+
+    win.removeMenu();
+
+    if (isDev)
+        win.loadURL('http://localhost:3000');
+    else
+        win.loadFile(`file://${path.join(__dirname, '../build/index.html')}`);
+
+    // win.loadURL(
+    //     isDev
+    //         ? 'http://localhost:3000'
+    //         : `file://${path.join(__dirname, '../build/index.html')}`
+    // );
+
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform != 'darwin')
+        app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0)
+        createWindow();
+});
---
+++ ai-road-marking-app/src/components/component-drop-file-input/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DropFileInput extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    };
+};
+
+export default DropFileInput;
