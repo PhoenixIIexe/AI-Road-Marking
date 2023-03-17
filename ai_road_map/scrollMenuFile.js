b3ff5dfd28c3105151b7d5a03100d37518790492
PhoenixIIexe <sashaerg10.07.6@mail.ru>
2023-03-17 16:21:42 +0300

    update animation menu-file

--- ai_road_map/scrollMenuFile.js
+++ ai_road_map/scrollMenuFile.js
@@ -1,32 +1,31 @@
 const menuFile = document.getElementsByClassName("menu-file");
 
 const setFileStyle = (file, index, scale = 0, marginTop = 0, zIndex = 2, background = "rgba(248, 248, 248, 0.2)", backdropFilter = "blur(25px)") => {
-    file[index].style.scale = scale;
-    file[index].style.marginTop = marginTop;
-    file[index].style.zIndex = zIndex;
-    file[index].style.background = background;
-    file[index].style.backdropFilter = backdropFilter;
+    if (typeof (file[index]) != 'undefined') {
+        file[index].style.scale = scale;
+        file[index].style.top = marginTop;
+        file[index].style.zIndex = zIndex;
+        file[index].style.background = background;
+        file[index].style.backdropFilter = backdropFilter;
+    }
 }
 
 const isFileVisible = () => {
-    const position = 30 + menuFile[0].scrollTop;
     const files = menuFile[0].getElementsByClassName('menu-file__item');
-    const excludedIndexes = [0 + Math.floor(position / 67), 1 + Math.floor(position / 67), 2 + Math.floor(position / 67)];
-    setFileStyle(files, excludedIndexes[0], 1);
-    setFileStyle(files, excludedIndexes[1], 1);
-    const scaleNewElement = 0.9 + 0.1 / 67 * (position % 67);
-    const marginTopNewElement = `-${67 - (position % 67)}px`;
-    const backgroundNewElement = `rgba(248, 248, 248, ${0.1 + 0.1 / 67 * (position % 67)})`;
-    const backdropFilterNewElement = `blur(${50 - 25 / 67 * (position % 67)}px)`;
-    console.log(backgroundNewElement, backdropFilterNewElement)
-    setFileStyle(files, excludedIndexes[2], scaleNewElement, marginTopNewElement, 1, backgroundNewElement, backdropFilterNewElement);
-    for (let i = 0; i < files.length; ++i) {
-        if (excludedIndexes[0] - 1 >= 0 && 67 - (position % 67))
-            setFileStyle(files, excludedIndexes[0] - 1, 1);
-        if (!(excludedIndexes.includes(i))) {
-            setFileStyle(files, i);
-        }
-    }
+    if (typeof (isFileVisible.counter) == 'undefined')
+        isFileVisible.counter = [0, 1, 2];
+    setFileStyle(files, isFileVisible.counter[1]);
+    setFileStyle(files, isFileVisible.counter[2]);
+    const heightBlock = 68;
+    const position = 30 + menuFile[0].scrollTop;
+    isFileVisible.counter = [0 + Math.floor(position / heightBlock), 1 + Math.floor(position / heightBlock), 2 + Math.floor(position / heightBlock)];
+    setFileStyle(files, isFileVisible.counter[0], 1);
+    setFileStyle(files, isFileVisible.counter[1], 1);
+    const scaleNewElement = 0.9 + 0.1 / heightBlock * (position % heightBlock);
+    const marginTopNewElement = `-${heightBlock - (position % heightBlock)}px`;
+    const backgroundNewElement = `rgba(248, 248, 248, ${0.1 + 0.1 / heightBlock * (position % heightBlock)})`;
+    const backdropFilterNewElement = `blur(${50 - 25 / heightBlock * (position % heightBlock)}px)`;
+    setFileStyle(files, isFileVisible.counter[2], scaleNewElement, marginTopNewElement, 1, backgroundNewElement, backdropFilterNewElement);
 };
 
 document.addEventListener("DOMContentLoaded", isFileVisible);
