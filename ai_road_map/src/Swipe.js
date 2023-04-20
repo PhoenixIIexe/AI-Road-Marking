377994a3b727edea12ae3924bec4de88326ad329
PhoenixIIexe <sashaerg10.07.6@mail.ru>
2023-04-20 11:19:32 +0300

    clip not work

--- ai_road_map/src/Swipe.js
+++ ai_road_map/src/Swipe.js
@@ -4,22 +4,19 @@ const swipeTitleHeight = swipeTitle.offsetHeight;
 const swipeImportFile = document.getElementById("swipeImportFile");
 const swipeImportFileHeight = swipeImportFile.offsetHeight;
 const menuFile = document.getElementById("menuFile");
+const menuFileHeight = swipeImportFile.offsetHeight;
+const uploadedFile = document.getElementById("uploadedFile");
 
 const swiping = () => {
     const positionScroll = swipe.scrollTop;
-    console.log(positionScroll);
     swipeTitle.style.opacity = 1 - positionScroll / swipeTitleHeight;
-    swipeTitle.style.transform = `translateY(-${positionScroll / 2}px) scaleY(${1 - positionScroll / swipeTitleHeight})`;
+    // swipeTitle.style.transform = `translateY(-${positionScroll / 2}px) scaleY(${1 - positionScroll / swipeTitleHeight})`;
+    let ctx = swipeTitle.getContext("2d")
+    ctx.rect(auto, auto, auto, swipeTitleHeight - positionScroll);
+    ctx.clip();
     const deltaImportFile = Math.max(0, positionScroll - swipeImportFileHeight + swipeTitleHeight);
     swipeImportFile.style.opacity = 1 - deltaImportFile / swipeImportFileHeight;
-    swipeImportFile.style.transform = `translateY(-${deltaImportFile / 2}px) scaleY(${1 - deltaImportFile / swipeImportFileHeight})`;
-    // if (deltaImportFile > swipeImportFileHeight) {
-    //     swipe.style.overflowY = "hidden";
-    //     menuFile.style.overflowY = "scroll";
-    // } else {
-    //     swipe.style.overflowY = "scroll";
-    //     menuFile.style.overflowY = "hidden";
-    // }
+    // swipeImportFile.style.transform = `translateY(-${deltaImportFile / 2}px) scaleY(${1 - deltaImportFile / swipeImportFileHeight})`;
 }
 
 document.addEventListener("DOMContentLoaded", swiping);
