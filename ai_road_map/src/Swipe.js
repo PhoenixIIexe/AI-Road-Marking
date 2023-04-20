b9c1741fee1b2e63eff58c8103f208c64c729bc2
PhoenixIIexe <sashaerg10.07.6@mail.ru>
2023-04-20 17:41:17 +0300

    test mask image

--- ai_road_map/src/Swipe.js
+++ ai_road_map/src/Swipe.js
@@ -2,6 +2,7 @@ const swipe = document.getElementById("swipe");
 const swipeTitle = document.getElementById("swipeTitle");
 const swipeTitleHeight = swipeTitle.offsetHeight;
 const swipeImportFile = document.getElementById("swipeImportFile");
+const swipeImportFileWidth = swipeImportFile.offsetWidth;
 const swipeImportFileHeight = swipeImportFile.offsetHeight;
 const menuFile = document.getElementById("menuFile");
 const menuFileHeight = swipeImportFile.offsetHeight;
@@ -11,12 +12,13 @@ const swiping = () => {
     const positionScroll = swipe.scrollTop;
     swipeTitle.style.opacity = 1 - positionScroll / swipeTitleHeight;
     // swipeTitle.style.transform = `translateY(-${positionScroll / 2}px) scaleY(${1 - positionScroll / swipeTitleHeight})`;
-    let ctx = swipeTitle.getContext("2d")
-    ctx.rect(auto, auto, auto, swipeTitleHeight - positionScroll);
-    ctx.clip();
+    swipeTitle.style.webkitMaskPositionY = `-${positionScroll}px`;
     const deltaImportFile = Math.max(0, positionScroll - swipeImportFileHeight + swipeTitleHeight);
     swipeImportFile.style.opacity = 1 - deltaImportFile / swipeImportFileHeight;
     // swipeImportFile.style.transform = `translateY(-${deltaImportFile / 2}px) scaleY(${1 - deltaImportFile / swipeImportFileHeight})`;
 }
 
 document.addEventListener("DOMContentLoaded", swiping);
+
+swipeTitle.style.maskSize = `${swipeImportFileWidth}px ${swipeTitleHeight + 20}px`;
+swipeTitle.style.webkitMaskSize = `${swipeImportFileWidth}px ${swipeTitleHeight + 20}px`;
